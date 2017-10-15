fbe4c338daf126dab5788de56efb1847136e11d7
nghiaminhle <admin@LAP00332s-MacBook-Pro.local>
2017-10-15 10:20:19 +0700

    fix bug shift index

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class MedianStore:
         else:
             appended_node = self.root.add_node(node)
             index_of_append_node = self.value_index[appended_node.label]
+            
             if appended_node.value < number:
                 if index_of_append_node < self.count - 1:
                     self.shift_index(index_of_append_node + 1)
@@ -24,13 +25,13 @@ class MedianStore:
                 self.shift_index(index_of_append_node)
                 self.index_values[index_of_append_node] = node
                 self.value_index[node.label] = index_of_append_node
-
+            
         self.count += 1
 
     def shift_index(self, from_index):
         t1 = self.index_values[from_index]
         t2 = None
-        for i in range(from_index, self.count):
+        for i in range(from_index, self.count+1):
             if i < self.count - 1:
                 t2 = self.index_values[i + 1]
             self.index_values[i + 1] = t1
@@ -46,21 +47,54 @@ class MedianStore:
         else:
             return self.index_values[center].value
 
-def main():
-    median_store = MedianStore()
-    median_store.add_number(7)
-    median_store.add_number(3)
-    median_store.add_number(6)
-    median_store.add_number(4)
-    median_store.add_number(2)
-    median_store.add_number(2)
-    median_store.add_number(5)
+def test():
+    testcases = [
+        [[1, 3, 9, 5, 7, 5, 0, 4, 3, 2, 9, 1, 0], [1, 2, 3, 4, 5, 5, 5, 4.5, 4, 3.5, 4, 3.5, 3]],
+        [[6, 1, 2], [6, 3.5, 2]],
+        [[3, 9, 2, 5, 2], [3, 6, 3, 4, 3]],
+        [[5, 5, 5, 5], [5, 5, 5, 5]],
+        [[9, 6, 4, 3, 1], [9, 7.5, 6, 5, 4]],
+        [[1, 2, 3, 4], [1, 1.5, 2, 2.5]],
+        [[1, 3, 2], [1, 2, 2]],
+        [[1], [1]]
+    ]
 
-    print('median:', median_store.get_median())
+    for case in testcases:
+        median_list = MedianStore()
+        print('Executing test case %s' % case[0])
+        for k, number in enumerate(case[0]):
+            median_list.add_number(number)
+            assert median_list.get_median() == case[1][k]
+
+def print_tree(root: Node):
+    if root != None:
+        print_tree(root.left)
+        print(root.label, root.value)
+        print_tree(root.rigt)
+
+def main():
+    #median_store = MedianStore()
+    #median_store.add_number(3)
+    #median_store.add_number(9)
+    #median_store.add_number(2)
+    #median_store.add_number(5)
+    #median_store.add_number(2)
+    
+    #median_store.add_number(2)
+    #median_store.add_number(2)
+    #median_store.add_number(5)
+    
+    #print_tree(median_store.root)
 
     #for i in range(0, median_store.count):
     #    print(median_store.index_values[i].value)
+    
+    #print('median:', median_store.get_median())
+    
+    test()
+    
 
+    
 
 if __name__ == "__main__":
     main()
